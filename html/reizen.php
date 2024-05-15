87a8c1171817af62605c87d4d24699544c0597c2
Pim Melchers <1210025@student.roc-nijmegen.nl>
2024-05-15 13:37:49 +0200

    Reizen database

--- html/reizen.php
+++ html/reizen.php
@@ -5,6 +5,27 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php while ($result = $stmt->fetch()) {?>
+        <div class="reis">
+            <div class="reis-naam">
+                <?= $result['Reisnaam']?>
+            </div>
+            <div class="reis-omschrijving">
+                <?= $result['Omschrijving']?>
+            </div>
+            <div class="reis-land">
+                <?= $result['Land']?>
+            </div>
+            <div class="reis-stad">
+                <?= $result['Stad']?>
+            </div>
+            <div class="reis-prijs">
+                <?= "€ ". $result['Prijs']?>
+            </div>
+            <div class="reis-duur">
+                <?= $result['Duur']?>
+            </div>
+        </div>
+    <?php }?>
 </body>
 </html>
