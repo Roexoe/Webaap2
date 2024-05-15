182dda1c3d1a36a07f6d3d6b614fbcdb1c2fd34d
Roexoe <maxfp.van.rooijen@gmail.com>
2024-05-15 13:12:50 +0200

    Header basis gemaakt met css

--- html/header.php
+++ html/header.php
@@ -3,12 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../styling/style.css">
+    <link rel="stylesheet" href="styling/style.css">
     <title>Test Document</title>
 </head>
 <body>
-    <div class="bruh-moment">
-        bruh moment
-    </div>
+    <header>
+        <div class="logo">
+            <img src="img/k3 Logo black.png" alt="Reislogo">
+        </div>
+        <div class="top-content">
+            <div class="top-text">
+                <span>Website van het jaar 1935!</span>
+                <span>Al meer dan 206 jaar ervaring</span>
+                <span>€1.600 Boekingskosten p.p.</span>
+            </div>
+            <div class="buttons">
+                <button>Reizen</button>
+                <button>Over ons</button>
+                <button>Contact</button>
+            </div>
+        </div>
+        <div class="profile">
+            <!-- Insert profile image or icon here -->
+            <button>Login</button>
+        </div>
+    </header>
 </body>
 </html>
