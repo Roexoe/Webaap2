8a03b81f669917db37f7c7b282e0d14327b0b6f8
Pim Melchers <1210025@student.roc-nijmegen.nl>
2024-05-28 12:04:49 +0200

    over ons
    Over ons pagina

--- html/overons.php
+++ html/overons.php
@@ -10,6 +10,31 @@ include_once("header.php");
     <title>Document</title>
 </head>
 <body>
-    
+<div class="tekstblok">
+    <div class="titel">Wie zijn wij?</div>
+    <div class="tekst">Welkom bij K3 Reizen, uw vertrouwde partner in onvergetelijke reiservaringen! Bij K3 Reizen draait alles om het creëren van magische momenten en avontuurlijke ontdekkingen. Wij zijn een toegewijd reisbureau met een passie voor het samenstellen van reizen die niet alleen voldoen aan uw verwachtingen, maar deze ook overtreffen. Of u nu droomt van een ontspannen strandvakantie, een avontuurlijke trektocht door de bergen, of een culturele stedentrip, wij staan klaar om uw perfecte reis te realiseren. Onze Missie Onze missie is simpel: wij willen u helpen de wereld te verkennen op een manier die past bij uw wensen en behoeften. We streven ernaar om reizen te organiseren die authentieke ervaringen bieden, waarbij elke bestemming tot leven komt door de ogen van onze ervaren reisdeskundigen.
+    </div>
+    <div class="tekst">
+    <div class="titel">Waarom K3 Reizen?</div> 1. Persoonlijke Service: Bij K3 Reizen krijgt u geen standaardpakket. Wij luisteren naar uw wensen en stellen een reis op maat samen, precies zoals u het voor ogen heeft. 2. Expertise en Ervaring: Ons team bestaat uit gepassioneerde reisexperts met jarenlange ervaring. Wij kennen de beste bestemmingen, verborgen parels en lokale hotspots die uw reis extra bijzonder maken. 3. Betrouwbaarheid: Uw comfort en veiligheid staan voorop. We werken samen met betrouwbare partners en zorgen ervoor dat alles tot in de puntjes geregeld is. 4. Unieke Ervaringen: Van exclusieve tours en lokale gidsen tot bijzondere accommodaties en unieke activiteiten – wij zorgen voor een onvergetelijke ervaring.
+    </div>
+    <div class="titel">Onze Diensten</div>
+    <div class="tekst">
+    - Maatwerkreizen: Elke reis die we plannen is uniek en wordt volledig afgestemd op uw voorkeuren. - Groepsreizen: Samen op avontuur met vrienden, familie of collega’s? Wij organiseren groepsreizen die perfect aansluiten bij uw gezelschap. - Last-minute aanbiedingen: Spontane reislust? Ontdek onze aantrekkelijke last-minute deals en vertrek binnen no-time naar uw droombestemming. - Zakelijke reizen: Voor zakelijke trips zorgen wij voor een naadloze en stressvrije ervaring, zodat u zich kunt concentreren op uw werk.
+    </div>
+    <div class="tekst">
+    <div class="titel">Neem Contact Op</div>
+    Heeft u vragen, wensen of bent u klaar om uw volgende avontuur te plannen? Ons team staat klaar om u te helpen. Neem vandaag nog contact met ons op en laat K3 Reizen uw dromen waarmaken.
+
+    ---
+
+    K3 Reizen
+    Donalduckstraat 21
+    0651737426
+    [E-mai
+    [Website URL]
+    </div>
+
+</div>
+<img class="hoofdkantoor" src="img/hoofdkantoor.jpg" alt="Hoofdkantoor">
 </body>
 </html>
